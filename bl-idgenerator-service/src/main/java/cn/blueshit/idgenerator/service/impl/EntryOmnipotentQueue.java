b1eacd4a4034c54b813243ccab751c29d3ad97d7
zhaoheng9 <zhaoheng@jd.com>
2016-07-11 17:51:14 +0800

    填充h2

--- bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/service/impl/EntryOmnipotentQueue.java
+++ bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/service/impl/EntryOmnipotentQueue.java
@@ -1,9 +1,59 @@
 package cn.blueshit.idgenerator.service.impl;
 
+import cn.blueshit.idgenerator.domain.SequenceId;
+import cn.blueshit.idgenerator.util.quene.OmnipotentQueue;
+
+import java.util.List;
+
 /**
  * Created by zhaoheng on 2016/7/11.
- * 入口队列..刚启动时,从mysql数据库中查询当前序列号
+ * 入口队列..刚启动时或者需要补充h2时,从mysql数据库中查询当前序列号
  */
-public class EntryOmnipotentQueue {
+public class EntryOmnipotentQueue implements OmnipotentQueue<SequenceId> {
 
+
+    @Override
+    public String getName() {
+        return "入口Queue";
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("入口Queue不支持此方法");
+    }
+
+    @Override
+    public List<SequenceId> peekFromIndex(int count, long fromIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean putToLast(SequenceId element) {
+        return false;
+    }
+
+    @Override
+    public SequenceId takeFromFirst() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(SequenceId element) {
+        return false;
+    }
+
+    @Override
+    public SequenceId peekFirst() {
+        return null;
+    }
+
+    @Override
+    public SequenceId peekLast() {
+        return null;
+    }
+
+    @Override
+    public boolean clear() {
+        return false;
+    }
 }
--- bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/util/quene/OmnipotentQueue.java
+++ bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/util/quene/OmnipotentQueue.java
@@ -1,13 +1,35 @@
 package cn.blueshit.idgenerator.util.quene;
 
+import java.util.List;
+
 /**
  * Created by zhaoheng on 2016/7/11.
  * 队列的顶级接口
  * dbquence->H2
  * memoryquence-->linkedblockquence
  */
-public interface OmnipotentQueue<T extends Element> {
+public interface OmnipotentQueue<E extends Element> {
 
+
+    //队列名称
+    String getName();
+
+    //队列大小..EntryOmnipotentQueue入口quence不支持
+    int size();
+
+    List<E> peekFromIndex(int count, long fromIndex);
+
+    boolean putToLast(E element);
+
+    E takeFromFirst();
+
+    boolean remove(E element);
+
+    E peekFirst();
+
+    E peekLast();
+
+    boolean clear();
 
 
 }
