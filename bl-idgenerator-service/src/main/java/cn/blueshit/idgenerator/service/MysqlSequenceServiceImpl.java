5f46a9cb08215a46b721783c9b32ae2f4283c0ac
zhaoheng9 <zhaoheng@jd.com>
2016-07-14 11:21:01 +0800

    测试版

--- bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/service/MysqlSequenceServiceImpl.java
+++ bl-idgenerator-service/src/main/java/cn/blueshit/idgenerator/service/MysqlSequenceServiceImpl.java
@@ -3,6 +3,10 @@ package cn.blueshit.idgenerator.service;
 import cn.blueshit.idgenerator.dao.SequenceDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * Created by zhaoheng on 2016/7/11.
@@ -13,6 +17,7 @@ public class MysqlSequenceServiceImpl implements SequenceService {
 
     private static Logger logger = LoggerFactory.getLogger(MysqlSequenceServiceImpl.class);
 
+    private PlatformTransactionManager transactionManager;
 
     /**
      * Mysql分库个数
@@ -29,20 +34,38 @@ public class MysqlSequenceServiceImpl implements SequenceService {
         this.sequenceDao = sequenceDao;
     }
 
+    /**
+     * 定义事物
+     */
+    protected TransactionStatus initTansactionStatus(PlatformTransactionManager transactionManager, int propagetion) {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(propagetion);
+        return transactionManager.getTransaction(def);
+    }
+
+
 
     /**
      * Mysql实现 id序列增加
      * 在memory 获取失败时走此方法
-     *
+     *事务必须
      * @param sequenceName
      * @return
      */
     @Override
     public Long getNextVal(String sequenceName) {
+        TransactionStatus transactionStatus = null;
         Long val = null;
-        int changeCount = sequenceDao.autoIncrease(sequenceName);
-        if (changeCount == 1) {
-            val = sequenceDao.getCurrVal(sequenceName);
+        try {
+            transactionStatus = this.initTansactionStatus(transactionManager, TransactionDefinition.PROPAGATION_REQUIRED);
+            int changeCount = sequenceDao.autoIncrease(sequenceName);
+            if (changeCount == 1) {
+                val = sequenceDao.getCurrVal(sequenceName);
+            }
+            transactionManager.commit(transactionStatus);
+        } catch (Exception e) {
+            transactionManager.rollback(transactionStatus);
+            logger.error("Mysql实现 id序列增加出错,exception:{}", e.getMessage());
         }
         return val;
     }
@@ -56,6 +79,8 @@ public class MysqlSequenceServiceImpl implements SequenceService {
      */
     @Override
     public Long[] getBatchNextVal(String sequenceName, int count) {
+        TransactionStatus transactionStatus = null;
+        Long lastVal = null;
         //========================================
         //可以尝试用threadlock防不同的数据源,不同的数据源有不同的transactionManager
         //根据数据源来获取相应的transactionmanager来手动管理事务
@@ -67,10 +92,16 @@ public class MysqlSequenceServiceImpl implements SequenceService {
         //例: count=10 shardsize=4
         int increment = shardSize * count;//增加后的值 需要乘以 分表的数量
         //因为事务的存在 这里先update再select 不会出现并发问题-->where条件后一定要带更新列的主键,主键行锁,非主键表锁
-        int changeCount = sequenceDao.increase(sequenceName, increment);
-        Long lastVal = null;
-        if (changeCount > 0) {
-            lastVal = sequenceDao.getCurrVal(sequenceName);
+        try {
+            transactionStatus = this.initTansactionStatus(transactionManager, TransactionDefinition.PROPAGATION_REQUIRED);
+            int changeCount = sequenceDao.increase(sequenceName, increment);
+            if (changeCount > 0) {
+                lastVal = sequenceDao.getCurrVal(sequenceName);
+            }
+            transactionManager.commit(transactionStatus);
+        } catch (Exception ex) {
+            transactionManager.rollback(transactionStatus);
+            logger.error("Mysql实现 id序列getBatchNextVal出错,exception:{}", ex.getMessage());
         }
         Long[] batchVal = null;
         if (lastVal != null) {
@@ -103,5 +134,13 @@ public class MysqlSequenceServiceImpl implements SequenceService {
 
     public SequenceDao getSequenceDao() {
         return sequenceDao;
+    }
+
+    public PlatformTransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 }
